3581f223892ef971980c6bb5cd46568106afb574
ltjax <marius.elvert@googlemail.com>
2020-05-17 20:06:45 +0200

    Rewrite travis files

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,26 +4,20 @@ set -e
 set -x
 
 if [[ "$(uname -s)" == 'Linux' ]]; then
-    sudo apt-get install -y libasound2-dev libpulse-dev
+    # Also install -multilib to cross-compile from x86-64 to x86
+    # https://stackoverflow.com/questions/4643197/missing-include-bits-cconfig-h-when-cross-compiling-64-bit-program-on-32-bit
+    sudo apt-get install gcc-${GCC_VERSION} g++-${GCC_VERSION} gcc-${GCC_VERSION}-multilib g++-${GCC_VERSION}-multilib libudev-dev 
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
+    sudo update-alternatives --config gcc
+    python3 --version
+    sudo pip3 install cmake==3.13.3
+    pip3 install --upgrade pip --user
+    pip --version
+    pip install conan --upgrade --user
+    pip install conan_package_tools --user
+    cmake --version
+    conan --version
+    conan profile new default --detect --force
+    conan profile update settings.compiler.libcxx=libstdc++11 default
 fi
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
-    brew install cmake || true
-
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
-
-    pyenv install 3.7.1
-    pyenv virtualenv 3.7.1 conan
-    pyenv rehash
-    pyenv activate conan
-fi
-
-pip install conan --upgrade
-pip install conan_package_tools bincrafters_package_tools
-
-conan user
